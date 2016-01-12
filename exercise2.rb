fbecf6129b32fc9a5aa9f9d0556a85a6a66db478
tanner466 <tanner466@gmail.com>
2016-01-12 13:52:21 -0500

    Commit exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,7 @@
+puts "You should tip #{55 * 0.2}" #20% tip
+
+puts "I am " + 24.to_s + " years old"
+
+puts "The result is #{45628 * 7839}"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)  #This is true because of not false for the second part
